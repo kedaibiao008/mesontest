3bdd3d213a2395c558f9c36e81b517a9e2873d30
kedaibiao008 <118370965+kedaibiao008@users.noreply.github.com>
2023-01-03 20:41:31 +0800

    Update mesontest.sh

--- mesontest.sh
+++ mesontest.sh
@@ -5,4 +5,4 @@ vim mesontest.sh
 wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.18/meson_cdn-linux-amd64.tar.gz' && tar -zxf meson_cdn-linux-amd64.tar.gz && rm -f meson_cdn-linux-amd64.tar.gz && cd ./meson_cdn-linux-amd64 && sudo ./service install meson_cdn
 sudo ./meson_cdn config set --token=lbbgsatngbqmtmpeazbyjtll --https_port=443 --cache.size=30
 sudo ./service start meson_cdn
-done
+
